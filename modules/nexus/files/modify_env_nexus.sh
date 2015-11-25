11dcea2dc15e10a13e0ccb122d478d2b50b665d5
mattcopas <mattwfcopas@gmail.com>
2015-11-25 14:49:59 +0000

    ECPP-1 #comment Nexus will install - problems writing pid without root when running console.

---
+++ modules/nexus/files/modify_env_nexus.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cat environment >> environment
+
+echo NEXUS_HOME="/usr/local/nexus-2.11.4-01/bin"
--- modules/nexus/manifests/init.pp
+++ modules/nexus/manifests/init.pp
@@ -23,34 +23,67 @@ class nexus {
   file { 'nexus_tar':
     ensure => 'file',
 	source => 'puppet:///modules/nexus/nexus-latest-bundle.tar.gz',
-	path   => '/usr/local',
+	path   => '/usr/local/nexus-latest-bundle.tar.gz',
 	notify => Exec['unzip_nexus'],
   }
   
   exec { 'unzip_nexus':
     cwd    => '/usr/local',
-	comand => 'tar -zxvf nexus-latest-bundle.tar.gz',
-	notify => Exec['modify_env_nexus'],
+	command => 'tar -zxvf nexus-latest-bundle.tar.gz',
+	notify => File['modify_env_nexus_script'],
+  }
+  
+  file { 'modify_env_nexus_script':
+    ensure => 'file',
+	source => 'puppet:///modules/nexus/modify_env_nexus.sh',
+	path   => '/usr/local/bin/modify_env_nexus_script.sh',
+	mode   => '0744',
+	notify => Exec['modify_env_nexus_run'],
+  }
+  
+  exec { 'modify_env_nexus_run':
+    command => '/usr/local/bin/modify_env_nexus_script.sh',
+	notify  => File['source_environment_script'],
   }
   
   # Change this to a bash script to execute?
-  exec { 'modify_env_nexus':
-    cwd     => '/etc/',
-	command => [
-	  'cat environment >> environment',
-	  "echo NEXUS_HOME='/usr/local/nexus-2.11.4-01/bin'"
-	],
-	notify => Exec['source_environment'],
+  #exec { 'modify_env_nexus':
+  # cwd     => '/etc/',
+  #	command => [
+  #	  'cat environment >> environment',
+  #	  "echo NEXUS_HOME='/usr/local/nexus-2.11.4-01/bin'"
+  #	],
+  #	notify => Exec['source_environment'],
+  #}
+  
+  file { 'source_environment_script':
+    ensure => 'file',
+	source => 'puppet:///modules/nexus/source_environment.sh',
+	path   => '/usr/local/bin/source_environment.sh',
+	mode   => '0744',
+	notify => Exec['source_environment_run'],
   }
   
-  exec { 'source_environment':
-	command => 'source /etc/environment',
+  exec { 'source_environment_run':
+    command => '/usr/local/bin/source_environment.sh',
 	notify  => Exec['link_nexus'],
   }
+  
+#  exec { 'source_environment':
+#	command => 'source /etc/environment',
+#	notify  => Exec['link_nexus'],
+#  }
   
   exec { 'link_nexus':
     cwd => '/usr/local',
 	command => 'ln -s nexus-2.11.4-01 nexus',
+	creates => '/usr/local/nexus-2.11.4-01/nexus',
+	notify  => Exec['change_nexus_permissions'],
+  }
+  
+  exec { 'change_nexus_permissions':
+    cwd     => '/usr/local/nexus-2.11.4-01/bin',
+	command => 'chmod a+x nexus',
   }
   
 }
