5eef4829680428e03bc65d6e4d3cf5789fb86025
MediterraneanDan <dds_9@hotmail.co.uk>
2015-11-26 17:09:00 +0000

    ECPP-2 #comment Added ability to eliminate jira lock but still doesnt open once installed on centos

--- modules/jira/manifests/init.pp
+++ modules/jira/manifests/init.pp
@@ -1,6 +1,5 @@
 class jira {
 
-
   Exec {
     path => [
       '/usr/local/bin',
@@ -12,6 +11,14 @@ class jira {
     logoutput => true,
   }
 
+  #this will ensure the node has wget
+  if ! defined(Package['jira-wget']) {
+    package { 'wget':
+      ensure => installed,
+      before => Exec['jira_mkdir'],
+    }
+  }
+  #this will make the directory for the download
   exec { 'jira_mkdir':
     unless  => 'test -e /opt/jirainstall',
     user    => root,
@@ -19,7 +26,7 @@ class jira {
     command => 'mkdir jirainstall',
     before  => Exec['jira_wget'],
   }
-
+  #this will change to download direcotry and download jira install file
   exec { 'jira_wget':
     # Check if file already exists
     unless  => 'test -f /opt/jirainstall/jira.bin',
@@ -29,25 +36,24 @@ class jira {
     timeout => 0,
     before  => Exec['jira_chmod'],
   }
-
+  #this will change permissions of the install file
   exec { 'jira_chmod':
     user    => root,
     cwd     => '/opt/jirainstall/',
     command => 'chmod a+x jira.bin',
-    before  => Exec['kill_jira'],
-
+    before  => Exec['jira_execute'],
   }
-
-  exec { 'kill_jira':
+  #this will kill any existing jira processes to free up the ports
+ # exec { 'kill_jira':
     # Check if jira process is running
-    unless  => 'pgrep -f "jira"',
-    command => 'pkill jira',
-    notify  => Exec['jira_execute'],
-  }
-
+  #  unless  => 'pgrep -f "jira"',
+  #  command => 'pkill jira',
+  #  notify  => Exec['jira_execute'],
+  #}
+  #this will 
   exec { 'jira_execute':
     # Check if jira process is running
-    unless  => 'pgrep -f "jira"',
+    #unless  => 'pgrep -f "jira"', 
     user    => root,
     cwd     => '/opt/jirainstall/',
     command => 'printf "\n2\n\n\n2\n8092\n8012\n\n" | ./jira.bin', #the ports may ha$
