f1585f754a008f822be0df6298452d5ee2128fe9
ExplodingTurkey <danielmabbett0@gmail.com>
2015-11-30 16:23:53 +0000

    ECPP-4 #comment Java now has operating system support for centos and ubuntu

--- modules/java/manifests/init.pp
+++ modules/java/manifests/init.pp
@@ -1,16 +1,29 @@
-# 
 class java {
   
-  if ! defined(Package['default-jre']) {
-    package { 'default-jre':
-      ensure => 'installed',
-    }
-  }
+#------------------------------------------------------------------
+#  The java executables
+#------------------------------------------------------------------
+
+  if $operatingsystem == 'Ubuntu' {
+
+    #This should install both jdk and jre as default-jdk comes with the other package
   
-  if ! defined(Package['default-jdk']) {
-    package { 'default-jdk':
-      ensure => 'installed',
-    }
+    if ! defined(Package['default-jdk']) {
+      package { 'default-jdk':
+        ensure => 'installed',
+      }
+    }	
+  
+  }elsif $operatingsystem == 'CentOS' {
+  
+    if ! defined(Package['java-1.7.0-openjdk']) {
+      package { 'java-1.7.0-openjdk':
+        ensure => installed,
+      }
+    }   
+
+  }else {
+    warning( 'Operating System is not supported.' )
   }
   
 }
