164227af6e08ff1067454e88ccfe5f7c52dd3cf5
ExplodingTurkey <danielmabbett0@gmail.com>
2015-12-03 13:45:22 +0000

    OS Conditional statements added -> not yet supporting centos - DM

--- modules/zabbix/manifests/init.pp
+++ modules/zabbix/manifests/init.pp
@@ -1,6 +1,16 @@
+#------------------------------------------------------------------
+# Author: Daniel Mabbett
+#------------------------------------------------------------------
+
 class zabbix {
+
   $zabip = $serverip
   $zabhost = $::fqdn
+
+  #----------------------------------------------------------------
+  # Path  
+  #----------------------------------------------------------------
+ 
   Exec {
     path => [
       '/usr/local/bin',
@@ -11,32 +21,42 @@ class zabbix {
     logoutput => true,
   }
 
-  exec {'install_agent_prezabbix_update':
-    user    => root,
-    command => 'apt-get update',
-    before  => Exec['install_agent_zabbix_software'],
-  }
+  #----------------------------------------------------------------
+  # Exec's  
+  #----------------------------------------------------------------
+ 
+  if $operatingsystem == 'Ubuntu' {
 
-  exec {'install_agent_zabbix_software':
-    user    => root,
-    command => 'apt-get install -y zabbix-agent',
-    before  => Exec['edit_zabbix_server'],
-  }
+    exec {'install_agent_prezabbix_update':
+      user    => root,
+      command => 'apt-get update',
+      before  => Exec['install_agent_zabbix_software'],
+    }
 
-  exec {'edit_zabbix_server':
-    user    => root,
-    command => "sed -i '85s/.*/Server=$zabip/' /etc/zabbix/zabbix_agentd.conf",
-    before  => Exec['edit_zabbix_hostname'],
-  }
+    exec {'install_agent_zabbix_software':
+      user    => root,
+      command => 'apt-get install -y zabbix-agent',
+      before  => Exec['edit_zabbix_server'],
+    }
+
+    exec {'edit_zabbix_server':
+      user    => root,
+      command => "sed -i '85s/.*/Server=$zabip/' /etc/zabbix/zabbix_agentd.conf",
+      before  => Exec['edit_zabbix_hostname'],
+    }
   
-  exec {'edit_zabbix_hostname':
-    user    => root,
-    command => "sed -i '137s/.*/Hostname=$zabhost/' /etc/zabbix/zabbix_agentd.conf",
-    before  => Exec['update_zabbix_config_files'],
-  }
+    exec {'edit_zabbix_hostname':
+      user    => root,
+      command => "sed -i '137s/.*/Hostname=$zabhost/' /etc/zabbix/zabbix_agentd.conf",
+      before  => Exec['update_zabbix_config_files'],
+    }
   
-  exec {'update_zabbix_config_files':
-    user    => root,
-    command => "service zabbix-agent restart",
-  }  
+    exec {'update_zabbix_config_files':
+      user    => root,
+      command => "service zabbix-agent restart",
+    }  
+
+  }else {
+    warning( 'Operating system is not supported.' )
+  }
 }
